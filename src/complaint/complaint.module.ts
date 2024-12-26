b4ffa0c449e6986ae96cfe7ca8abcdf9cad586ac
maryem012 <mariambenayed@outlook.com>
2024-12-26 21:58:14 +0100

    commiii

--- src/complaint/complaint.module.ts
+++ src/complaint/complaint.module.ts
@@ -7,9 +7,12 @@ import { complaintSchema } from './schema/complaint.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ComplaintController } from './complaint.controller';
 import { ComplaintService } from './complaint.service';
+import { NotificationModule } from 'src/notification/notification.module';
 
 @Module({
-    imports: [    MongooseModule.forFeature([{ name: 'complaint', schema: complaintSchema }])
+    imports: [    MongooseModule.forFeature([{ name: 'complaint', schema: complaintSchema }]),
+    NotificationModule// Ensure NotificationModule is imported here
+
 ],
     controllers: [ComplaintController],
     providers: [ComplaintService],
--- src/complaint/complaint.service.ts
+++ src/complaint/complaint.service.ts
@@ -3,10 +3,11 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { complaint } from './interface/complaint.interface';
 import { complaintDto } from './dto/complaint.dto';
+import { NotificationService } from 'src/notification/notification.service';
 
 @Injectable()
 export class ComplaintService {
-   constructor(@InjectModel('complaint') private readonly complaintModel: Model<complaint>,) { }
+   constructor(@InjectModel('complaint')  private readonly complaintModel: Model<complaint>,private readonly notificationService: NotificationService) { }
 
 async getcomplaints(): Promise<complaint[]> {
   const complaints = await this.complaintModel.find() ;
@@ -41,6 +42,11 @@ async updatecomplaint(complaintId: string, complaintDto: complaintDto): Promise<
   const existingcomplaint = await this.complaintModel.findByIdAndUpdate(complaintId, complaintDto, { new: true });
   if (!existingcomplaint) {
     throw new NotFoundException(`complaints #${complaintId} not found`);
+  }
+  if (complaintDto.status === 'treated') {
+    const userId = existingcomplaint.studentId; // Assuming there's a userId field in your request document
+    const message = 'Your complaint  has been treated.';
+    await this.notificationService.createNotification(userId, message);
   }
   return existingcomplaint;
 }}
